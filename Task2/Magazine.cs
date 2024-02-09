74922954eb9d77c029ae56a2d148141db442b927
Globlobglob <122725189+Globlobglob@users.noreply.github.com>
2024-02-09 12:38:16 +0200

    Add files via upload

---
+++ Task2/Magazine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Magazine : Press
+    {
+        string quality;
+        public string Quality { get => quality; set => quality = value; }
+
+        public Magazine() { }
+
+        public Magazine(int copies,string name,double price,string quality) :base(copies, name,price)
+        { 
+            this.quality = quality;
+        }
+
+        public new double Cost()
+        {
+            base.Cost();
+            if (quality == "высокое")
+            {
+                base.Cost();
+            }
+
+        }
+
+    }
+}
---
+++ Task2/Press.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Press
+    {
+        int copies;
+        string name;
+        double price;
+
+        private int Copies { get => copies; set => copies = value; }
+        private string Name { get => name; set => name = value; }
+        private double Price { get => price; set => price = value; }
+
+
+       public Press() { }
+       public Press(int copies,string name,double price) {
+        
+            this.copies = copies;
+            this.name = name;
+            this.price = price;
+       }
+
+        public double Cost()
+        {
+            return copies * price;
+        }
+
+        public void Output()
+        {
+            Console.WriteLine($"Copies:{Copies}\nName {Name}\n Price 1st {price}\nCost {Cost():f1}");
+        }
+    }
+}
